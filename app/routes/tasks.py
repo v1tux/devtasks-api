80bdc502b23fc3d5c6d75834bf4c3239603f1022
v1tux <victorlobprates@gmail.com>
2026-05-13 12:36:27 -0300

    feat: add task update endpoint and HTTP exceptions

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from app.schemas.task import TaskCreate
+from fastapi import APIRouter, HTTPException
+from app.schemas.task import TaskCreate, TaskUpdate
 
 router = APIRouter()
 
@@ -7,9 +7,16 @@ tasks = []
 task_id_counter = 1
 
 
-@router.get("/tasks")
-def list_tasks():
-    return tasks
+@router.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    
+    raise HTTPException(
+        status_code=404, 
+        detail="Task not found"
+    )
 
 
 @router.post("/tasks")
@@ -30,3 +37,19 @@ def create_task(task: TaskCreate):
         "message": "Task created successfully",
         "task": new_task
     }
+@router.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+
+            return {
+                "message": "Task updated successfully",
+                "task": task
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
 
+
 class TaskCreate(BaseModel):
     title: str
 
 
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
+    
 class TaskResponse(BaseModel):
     id: int
     title: str
